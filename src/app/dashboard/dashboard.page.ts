d9ac6c83ae358494d627db5e3ca826380393eaa7
CIST2932Spring2019 <william.ith13@gmail.com>
2019-04-08 17:19:33 -0400

    added patients and labs cards with navigation to dashboard

--- src/app/dashboard/dashboard.page.ts
+++ src/app/dashboard/dashboard.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  viewPatientsPage() {
+    this.router.navigate(['patients']);
+  }
+
+  viewLabsPage() {
+    this.router.navigate(['labs']);
   }
 
 }
