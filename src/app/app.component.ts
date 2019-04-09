e246c51cfebd67d40f1f56541e399c24d9d7cd21
CIST2932Spring2019 <william.ith13@gmail.com>
2019-04-09 14:08:40 -0400

    added icon tabs for navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -10,6 +11,11 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
+  isDashboard = false;
+  isPatients = false;
+  isLabs = false;
+  isPharmacists = false;
+
   public pages = [
     {
       title: 'Dashboard',
@@ -36,7 +42,8 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private router: Router
   ) {
     this.initializeApp();
   }
@@ -46,5 +53,35 @@ export class AppComponent {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
+  }
+
+  togglePage(whichPage: string): void {
+    this.isDashboard = false;
+    this.isPatients = false;
+    this.isLabs = false;
+    this.isPharmacists = false;
+
+    let newTab: string = '';
+
+    switch (whichPage) {
+      case 'dashboard':
+        this.isDashboard = true;
+        newTab = 'dashboard';
+        break;
+      case 'patients':
+        this.isPatients = true;
+        newTab = 'patients';
+        break;
+      case 'labs':
+        this.isLabs = true;
+        newTab = 'labs';
+        break;
+      case 'pharmacists':
+        this.isPharmacists = true;
+        newTab = 'pharmacists';
+        break;
+    }
+
+    this.router.navigate([newTab]);
   }
 }
