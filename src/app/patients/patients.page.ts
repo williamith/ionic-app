bfa800e5c92dec70645d6f633c0a4e5cef27056f
CIST2932Spring2019 <william.ith13@gmail.com>
2019-04-22 01:02:40 -0400

    improved search bar

--- src/app/patients/patients.page.ts
+++ src/app/patients/patients.page.ts
@@ -10,12 +10,16 @@ import { Router } from '@angular/router';
 })
 export class PatientsPage implements OnInit {
   patients = [];
+  loadedPatients = [];
 
   constructor(private patientsService: PatientsService, private router: Router) { }
 
   ngOnInit() {
     this.patientsService.readPatients().subscribe(
-      response => { this.patients = response; },
+      response => {
+        this.patients = response;
+        this.loadedPatients = response;
+      },
       error => console.log(error),
       () => { this.patients = this.patients.filter(function (patient) { return patient.isActive === true }); }
     );
@@ -33,5 +37,28 @@ export class PatientsPage implements OnInit {
   viewPatientEditPage(patient: Patient) {
     this.patientsService.patient = patient;
     this.router.navigate(['patients', 'patient-edit']);
+  }
+
+  initializeItems(): void {
+    this.patients = this.loadedPatients;
+  }
+
+  filterList(event: any) {
+    this.initializeItems();
+
+    const searchTerm = event.srcElement.value;
+
+    if (!searchTerm) {
+      return;
+    }
+
+    this.patients = this.patients.filter(patient => {
+      if (patient.lastName && searchTerm) {
+        if (patient.lastName.toLowerCase().indexOf(searchTerm.toLowerCase()) > -1) {
+          return true;
+        }
+        return false;
+      }
+    });
   }
 }
